79adeda3428d52024b4590cb7c0c1aea32426c06
Y.S <103623717+lili-ys@users.noreply.github.com>
2023-03-09 01:56:07 +0900

    init: 환경 설정

---
+++ src/pages/MainPage/Main.js
@@ -0,0 +1,11 @@
+import { MainWrapper } from "./main-styled";
+
+const Main = () => {
+  return (
+    <MainWrapper>
+      <div>main page</div>
+    </MainWrapper>
+  );
+};
+
+export default Main;
