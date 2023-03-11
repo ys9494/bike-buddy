ef7c9ce73656a987c64a3a2874a5c0d46c636939
Y.S <103623717+lili-ys@users.noreply.github.com>
2023-03-11 22:43:33 +0900

    feat: main

--- src/pages/MainPage/Main.js
+++ src/pages/MainPage/Main.js
@@ -1,9 +1,25 @@
-import { MainWrapper } from "./main-styled";
+import { useEffect, useState } from "react";
+import { MainWrapper, ChartWrapper } from "./main-styled";
 
 const Main = () => {
+  const [dateInput, setDateInput] = useState("");
+
+  useEffect(() => {
+    console.log(dateInput);
+  }, [dateInput]);
+
   return (
     <MainWrapper>
-      <div>main page</div>
+      <h1>main page</h1>
+      <ChartWrapper>
+        <div>
+          <input
+            type="datetime-local"
+            value={dateInput || ""}
+            onChange={(e) => setDateInput(e.target.value)}
+          />
+        </div>
+      </ChartWrapper>
     </MainWrapper>
   );
 };
