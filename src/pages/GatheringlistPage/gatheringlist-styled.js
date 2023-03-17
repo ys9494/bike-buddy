0b6e011c9bce97ccb6dbedc46c618be10d635a38
Ariel <kshhj2118@gmail.com>
2023-03-17 22:50:49 +0900

    feat: gathering

--- src/pages/GatheringlistPage/gatheringlist-styled.js
+++ src/pages/GatheringlistPage/gatheringlist-styled.js
@@ -2,9 +2,13 @@ import styled from "styled-components";
 
 export const GatheringlistWrapper = styled.div`
   display: flex;
-  margin: 10px 100px;
+  margin: 20px 400px;
   background-color: beige;
-  padding:50px;
+  padding: 40px;
   align-items: left;
-  border-radius: 30px;`;
+  border-radius: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  color: green;
+  `;
 
