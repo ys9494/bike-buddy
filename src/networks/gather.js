606c57e48839c7ffabfabeaa0e0a11d878d141e2
ashnamuh <ashnamuh@gmail.com>
2023-03-20 19:26:13 +0900

    Refactoring5: api instance 분리

--- src/networks/gather.js
+++ src/networks/gather.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import api from '../commons/api'
 
 export const fetchMyGatheringList = async () => {
     try {
-      const response = await axios.get("/api/gatherings");
+      const response = await api.get("/api/gatherings");
 
       return response
     } catch (error) {
