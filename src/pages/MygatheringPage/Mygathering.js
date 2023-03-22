37f1e600744da159a14121a2a0aa9394870d1da4
박성환 <summun865@gmail.com>
2023-03-22 15:27:46 +0900

    feat:login

--- src/pages/MygatheringPage/Mygathering.js
+++ src/pages/MygatheringPage/Mygathering.js
@@ -6,28 +6,26 @@ import {
 } from "./Mygathering-styled";
 import { useMemo } from "react";
 
-import { useMyGatheringList } from '../../hooks/gathering.hook'
+import { useMyGatheringList } from "../../hooks/gathering.hook";
 
 const Mygathering = () => {
   const me = {
     id: 1,
-    name: 'Ash Lee'
-  }
+    name: "Ash Lee",
+  };
 
-  const { myGatheringList } = useMyGatheringList()
+  const { myGatheringList } = useMyGatheringList();
 
   const appliedGatheringList = useMemo(() => {
-    return myGatheringList.filter(gathering => {
-      return gathering.createdBy.id !== me.id
-    })
-  }, [myGatheringList, me])
+    return myGatheringList.filter((gathering) => {
+      return gathering.createdBy.id !== me.id;
+    });
+  }, [myGatheringList, me]);
   const ownGatheringList = useMemo(() => {
-    return myGatheringList.filter(gathering => {
-      return gathering.createdBy.id === me.id
-    })
-  }, [myGatheringList, me])
-
-
+    return myGatheringList.filter((gathering) => {
+      return gathering.createdBy.id === me.id;
+    });
+  }, [myGatheringList, me]);
   return (
     <>
       <MygatheringWrapper>
@@ -62,7 +60,6 @@ const Mygathering = () => {
           <div>날짜</div>
           <div>대여소명</div>
           <button>참가취소</button>
-
           {ownGatheringList.map((item, index) => {
             return (
               <MygatheringItems key={item.id} className="key">
